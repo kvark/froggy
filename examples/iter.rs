fd18afff26ead52427eb6821d2b4e16cf4b62bd4
Ilya Bogdanov <fumlead@gmail.com>
2017-04-17 22:12:15 +0300

    Change: `iter` example (#5)
    Replace: useless aquirig of _ptr replaced with small example of
    finding and changing component in storage.

--- examples/iter.rs
+++ examples/iter.rs
@@ -4,15 +4,24 @@ fn main() {
     let storage = froggy::Storage::new();
     {
         let mut s = storage.write();
-        for &v in [5i32,7,4,6,7].iter() {
+        for &v in [5 as i32, 7, 4, 6, 7].iter() {
             s.create(v);
         }
     }
-    let _ptr = {
-        let s = storage.read();
-        let i = s.iter().position(|v| *v == 4).unwrap();
-        s.pin(i)
-    };
+    println!("Initial array:");
+    for value in storage.read().iter() {
+        println!("Value {}", value);
+    }
+    {
+        let ptr = {
+            let r = storage.read();
+            let index = r.iter().position(|v| *v == 4).unwrap();
+            r.pin(index).unwrap()
+        };
+        let mut w = storage.write();
+        *w.access(&ptr) = 350 as i32;
+    }
+    println!("Array after change:");
     for value in storage.read().iter() {
         println!("Value {}", value);
     }
