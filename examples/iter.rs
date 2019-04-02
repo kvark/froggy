49fc0e4985e7ed4edd90ff4a450d32ade194c4f4
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2019-04-03 00:21:43 +0300

    Fix iter example

--- examples/iter.rs
+++ examples/iter.rs
@@ -4,16 +4,16 @@ fn main() {
     let mut storage: froggy::Storage<i32> =
         [5, 7, 4, 6, 7].iter().cloned().collect();
     println!("Initial array:");
-    for value in storage.iter() {
+    for value in storage.iter_all() {
         println!("Value {}", *value);
     }
     let ptr = {
-        let item = storage.iter().find(|v| **v == 4).unwrap();
+        let item = storage.iter_all().find(|v| **v == 4).unwrap();
         storage.pin(&item)
     };
     storage[&ptr] = 350;
     println!("Array after change:");
-    for value in &storage {
+    for value in storage.iter_all() {
         println!("Value {}", *value);
     }
 }
